f1cd79aed676b59e36394a1754ac2e0fffc72d98
eliassz <lomonosov.iv@phystech.edu>
2024-06-27 11:44:27 +0000

    [update]

--- script/base_tree_script.py
+++ script/base_tree_script.py
@@ -1,16 +1,18 @@
+import argparse
 import pandas as pd
 from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import recall_score, precision_score
 import mlflow
 
 
-def main():
+def main(model_name):
     # Define feature names
     features = ['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid']
-
+    
     # Load the iris dataset
     iris = datasets.load_iris()
     iris_df = pd.DataFrame(iris.data, columns=features)
@@ -25,9 +27,22 @@ def main():
         x, y, test_size=test_size, random_state=1
     )
 
-    # Model parameters
-    model_name = 'RandomForestClassifier'
-    model = RandomForestClassifier(random_state=1)
+    # Dictionary of models
+    models = {
+        'RandomForestClassifier': RandomForestClassifier(random_state=1),
+        'DecisionTreeClassifier': DecisionTreeClassifier(random_state=1),
+        'LogisticRegression': LogisticRegression(random_state=1, max_iter=200)
+    }
+
+    # Check if the specified model is valid
+    if model_name not in models:
+        raise ValueError(f"Model '{model_name}' is not a valid model. \
+                          Choose from: {list(models.keys())}")
+
+    # Select the model
+    model = models[model_name]
+
+    # Train the model
     model.fit(train_x, train_y)
 
     # Make predictions
@@ -44,7 +59,7 @@ def main():
     mlflow.set_tracking_uri('http://84.201.128.89:90/')
     mlflow.set_experiment('ivlomonosov-seminar-pipeline')
 
-    with mlflow.start_run(run_name='tree'):
+    with mlflow.start_run(run_name=model_name):
         mlflow.log_metrics({
             'prec': prec,
             'recal': recal,
@@ -57,4 +72,13 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Train a model on the iris dataset \
+            and log metrics with MLflow'
+    )
+    parser.add_argument(
+        'model_name', type=str, help='The name of the model to train. \
+                        Choose from: RandomForestClassifier, \
+                        DecisionTreeClassifier, LogisticRegression')
+    args = parser.parse_args()
+    main(args.model_name)
